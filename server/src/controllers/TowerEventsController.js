00683f31aaae82f24c5320910f2ceec17b857e3e
Roman <romanjolsen@gmail.com>
2025-03-25 15:58:16 -0600

    edit event start

--- server/src/controllers/TowerEventsController.js
+++ server/src/controllers/TowerEventsController.js
@@ -1,23 +1,58 @@
 import { Auth0Provider } from "@bcwdev/auth0provider";
 import BaseController from "../utils/BaseController.js";
 import { towerEventService } from "../services/TowerEventService.js";
+import { dbContext } from "../db/DbContext.js";
 
 export class TowerEventsController extends BaseController {
   constructor() {
     super('api/events')
     this.router
-    .use(Auth0Provider.getAuthorizedUserInfo)
-    .post('', this.postEvent)
+      .get('', this.getEvents)
+      .get('/:eventId', this.getEventById)
+      .use(Auth0Provider.getAuthorizedUserInfo)
+      .post('', this.postEvent)
+      .put('/:eventId', this.editEvent)
 
 
   }
+async  editEvent(request, response, next) {
+try {
+  const newEventData = request.body
+  const id = request.params.eventId
+  
+  const user = request.userInfo
 
-  async postEvent(request, response, next){
+  const event = await towerEventService.editEvent(id, newEventData, user)
+  response.send(event)
+} catch (error) {
+  next(error)
+}
+  }
+async  getEventById(request,response,next) {
+  try {
+    const id = request.params.eventId
+    const event = await towerEventService.getEventById(id)
+    response.send(event)
+  } catch (error) {
+    next(error)
+  }
+  }
+  async getEvents(request, response, next) {
+    try {
+
+      const events = await towerEventService.getEvents()
+      response.send(events)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async postEvent(request, response, next) {
     try {
       const eventData = request.body
       const user = request.userInfo
       eventData.creatorId = user.id
-      
+
       const event = await towerEventService.postEvent(eventData)
       response.send(event)
     } catch (error) {
--- server/src/services/TowerEventService.js
+++ server/src/services/TowerEventService.js
@@ -1,12 +1,34 @@
 import { dbContext } from "../db/DbContext.js"
 
 class TowerEventService {
+  async editEvent(id, newEventData, user) { // refer to inspire lecture if lost
+    const event = await dbContext.TowerEvent.findById(id)
+    if (event.creatorId != user.id) {
+      return
+    }
+    event.equals(newEventData)
+    await event.save()
+
+    return event
+  }
+  async getEventById(id) {
+
+    const event = await dbContext.TowerEvent.findById(id).populate('creator')
+    return event
+  }
+  async getEvents() {
+
+    const events = await dbContext.TowerEvent.find().populate('creator')
+
+
+    return events
+  }
   async postEvent(eventData) {
 
-   const event = await dbContext.TowerEvent.create(eventData)
-   await event.populate('creator')
-  //  await event.populate('ticketCount')
-   return event
+    const event = await dbContext.TowerEvent.create(eventData)
+    await event.populate('creator')
+    //  await event.populate('ticketCount')
+    return event
 
 
   }
